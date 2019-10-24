cf1bfd914ac0d3f09beb6cdda96f6122b09c590c
Cameron Moir <cameronjmoir@gmail.com>
2019-10-24 13:56:37 +0100

    chart on screen

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,32 @@
 <template lang="html">
 <div>
   <h1>Fuel Usage</h1>
-
+  <h1>{{organisedData}}</h1>
+  <fuel-chart :organisedData="organisedData"></fuel-chart>
 </div>
 </template>
 
 <script>
+import FuelChart from './components/FuelChart.vue'
+
 export default {
   data(){
     return {
       data: [],
-      organisedData: []
+      organisedData: null
     }
   },
   components: {
-    // 'fuel-chart': FuelChart
-  },
-  methods: {
-    organiseData(){
-      console.log(this.organisedData.generationmix)
-    }
+    "fuel-chart": FuelChart
   },
   mounted(){
     fetch('https://api.carbonintensity.org.uk/generation')
     .then(res => res.json())
     .then((fetchedData) => {
-      this.data = fetchedData.data.generationmix;
+      this.data = fetchedData.data.generationmix
+      let mappedObj = this.data.map((obj) => Object.values(obj))
+      mappedObj.unshift(["fuel", "percentage"]);
+      this.organisedData = mappedObj
     }
     )
   }
---
+++ src/components/FuelChart.vue
@@ -0,0 +1,31 @@
+<template lang="html">
+  <div id="app" v-if="organisedData">
+    <GChart
+       type="ColumnChart"
+       :data="organisedData"
+       :options="chartOptions"
+     />
+  </div>
+</template>
+
+<script>
+import { GChart } from "vue-google-charts";
+
+export default {
+  name: "fuel-chart",
+  props: ["organisedData"],
+  data () {
+    return {
+      chartOptions: {
+        chart: {
+          title: 'Fuel Type Usage Rates',
+          subtitle: 'Over Selected Timeframe',
+        }
+      }
+    }
+  },
+}
+</script>
+
+<style lang="css" scoped>
+</style>
